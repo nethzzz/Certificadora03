1a5d319014f9c658df432a48e08d9bad8b8f06ad
Lucas Pelegrina <141710271+LsPelegrina@users.noreply.github.com>
2025-12-01 23:26:38 +0000

    Remoção token de listagem

--- Backend/routes/participantRoutes.js
+++ Backend/routes/participantRoutes.js
@@ -9,7 +9,7 @@ router.post('/', checkToken, isAdmin, ParticipantController.create);
 router.patch('/:id', checkToken, isAdmin, ParticipantController.updateParticipant);
 router.delete('/:id', checkToken, isAdmin, ParticipantController.removeParticipant);
 // Visualização e busca pública
-router.get('/', checkToken, ParticipantController.getAll);
+router.get('/', ParticipantController.getAll);
 router.get('/search', checkToken, ParticipantController.search);
 
 module.exports = router;
